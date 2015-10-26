be8b5ea08eaffb293203f6540079536f72b6c00e
George <georgewitteman@gmail.com>
2015-10-26 17:00:27 -0400

    Fix the options to save by pressing enter in the form

--- options.js
+++ options.js
@@ -1,16 +1,17 @@
 // Saves options to chrome.storage.sync.
 function save_options() {
-  var college = document.getElementById('college').value;
-  chrome.storage.sync.set({
-    college: college
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+    var college = document.getElementById('college').value;
+    chrome.storage.sync.set({college: college}, updateCollege());
+    return false;
+}
+
+function updateCollege() {
+      // Update status to let user know options were saved.
+      var status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 750);
 }
 
 // Restores select box with colleges
@@ -22,22 +23,16 @@ function restore_options() {
           document.getElementById('college').value = items["college"];
       } else {
           document.getElementById('college').value = "Vassar College";
-          chrome.storage.sync.set({
-            college: "Vassar College"
-          }, function() {
-            // Update status to let user know options were saved.
-            var status = document.getElementById('status');
-            status.textContent = 'Options saved.';
-            setTimeout(function() {
-              status.textContent = '';
-            }, 750);
-          });
+          chrome.storage.sync.set({college: "Vassar College"}, updateCollege());
       }
 
   });
-
-
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById('save').addEventListener("click", save_options);
+document.getElementById('college').addEventListener("keypress", function(e) {
+        if (e.keyCode === 13) {
+            save_options();
+            e.preventDefault();
+        }
+    }, false);
